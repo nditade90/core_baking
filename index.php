e555d672dd90f3dbd8595fbf867ae5fb3403ffd8
Black mytechnologies <thandize@gmail.com>
2023-01-25 17:40:50 +0200

    First commit projet in plesk

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Core Baking web site</title>
+</head>
+
+<body>
+
+    <h1> This is web site of core baking system</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, recusandae necessitatibus voluptatem voluptatibus minus eos? Soluta, blanditiis atque similique iure unde ex laborum consequatur in voluptatem distinctio adipisci quia sunt?</p>
+</body>
+</html>
